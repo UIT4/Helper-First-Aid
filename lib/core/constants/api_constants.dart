1796df77de3327de411ff4df93bcb705cc20c8e8
LASE3 <nijim.ahmad077@gmail.com>
2026-05-24 01:48:05 +0300

    ip

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,10 @@
 class ApiConstants {
 
-  static const String baseUrl = 'http://10.0.2.2/oeba/api';
+  static const String baseUrl = 'http://192.168.100.10/oeba/api';
 
   static const String syncIncidents = '$baseUrl/sync_incidents.php';
   static const String contentVersion = '$baseUrl/content_version.php';
   static const String contentPackage = '$baseUrl/content_package.php';
   static const String syncProfile = '$baseUrl/sync_profile.php';
+  static const String uploadProfileImage = '$baseUrl/upload_profile_image.php';
 }
