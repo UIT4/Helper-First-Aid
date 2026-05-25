50f38a621d56151915eaa4ed81aea580ee97408a
LASE3 <nijim.ahmad077@gmail.com>
2026-05-25 08:02:52 +0300

    api

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,20 +1,20 @@
 class ApiConstants {
   static const String baseUrl =
-      'http://192.168.100.10/HELPER-FIRST-AID-PHP-DASHBOARD-/api/content_version.php';
+      'http://192.168.100.10/HELPER-FIRST-AID-PHP-DASHBOARD-/api';
 
-  // AUTH
   static const String authLogin = '$baseUrl/auth_login.php';
   static const String authRegister = '$baseUrl/auth_register.php';
 
-  // INCIDENTS
+  static const String sendOtp = '$baseUrl/send_otp.php';
+  static const String verifyOtp = '$baseUrl/verify_otp.php';
+  static const String resetPassword = '$baseUrl/reset_password.php';
+
   static const String syncIncidents = '$baseUrl/sync_incidents.php';
   static const String uploadIncidentImage = '$baseUrl/upload_incident_image.php';
 
-  // CONTENT
   static const String contentVersion = '$baseUrl/content_version.php';
   static const String contentPackage = '$baseUrl/content_package.php';
 
-  // PROFILE
   static const String syncProfile = '$baseUrl/sync_profile.php';
   static const String uploadProfileImage = '$baseUrl/upload_profile_image.php';
 }
